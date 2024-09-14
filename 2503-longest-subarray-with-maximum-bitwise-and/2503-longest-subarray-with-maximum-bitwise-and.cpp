f659a1790fd814729e343f945b8e898a81f3ec83
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2024-09-15 00:08:29 +0530

    Time: 110 ms (36.84%), Space: 84.9 MB (34.21%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -3,31 +3,29 @@ public:
     int longestSubarray(vector<int>& nums) {
         int max_value = INT_MIN;
         int max_index = 0;
+        //getting the max value and the index of it
         for(int i=0;i<nums.size();i++){
             if(max_value<nums[i]){
                 max_value = nums[i];
                 max_index = i;
             }
         }
-        cout<<max_value<<" ";
-        cout<<max_index<<" ";
         int n = nums.size();
         int c=0;
-        vector<int>v;
+        int length = 0;
+
+        //starting from the index of max and counting the max 
+        //Edge case - [1,2,3,4,4,2,3,4,4,4,4]  then it 
+        // will count 4 two times only
         while(max_index<n){
             if(nums[max_index]==max_value){
                 c++;
-            }else if(nums[max_index]!=max_value){
-                v.push_back(c);
+                length=max(length,c);
+            }else{
                 c=0;
             }
             max_index++;
         }
-        v.push_back(c);
-        int m=v[0];
-        for(int i=0;i<v.size();i++){
-            m=max(m,v[i]);
-        }
-        return m;
+        return length;
     }
 };
