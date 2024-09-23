ec08dda4669e9cfafd1c056b6c78e8fe11ebfc83
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2024-09-23 15:03:01 +0530

    Time: 87 ms (12.01%), Space: 70.7 MB (22.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int l = nums.size();
-        int sum=0;
-        int res=0;
-        int maxi=INT_MIN;
-        for(int i =0;i<l;i++){
-            sum+=nums[i];
-            if(sum<0)sum=0;
+        int sum=nums[0];
+        int res=nums[0];
+        for(int i =1;i<l;i++){
+            // sum+=nums[i];
+            // if(sum<0)sum=0;            //whenever our sum becomes less than 0 get it 0;
+            sum=max(sum+nums[i],nums[i]);
             res=max(res,sum);
         }
-        int max_ele=INT_MIN;
-        if(res==0){
-            for(int i=0;i<l;i++){
-                max_ele=max(max_ele,nums[i]);
-            }
-            return max_ele;
-        }
+        // int max_ele=INT_MIN;
+
+        // //when there are all negative elements then our res will be zero 
+        // // but we have to give the max element in the nums so calculating it.
+        // if(res==0){                    
+        //     for(int i=0;i<l;i++){
+        //         max_ele=max(max_ele,nums[i]);
+        //     }
+        //     return max_ele;
+        // }
         return res;
     }
 };
