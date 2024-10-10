43f556c907ce64a536d5218fcc205d4932d6513d
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2024-10-11 00:37:59 +0530

    Update 1002-maximum-width-ramp.cpp
    added comments for better understanding

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
+        //BRUTE FORCE 
+        //USED NESTED LOOP ONE FROM START AND SECOND FROM THE END 
+        //AND WHENEVER WE ACCOUNTER OUR CONDITION THEN BREAK TC->O(N*N)  WHICH CAUSED TLE
+
+        //OPTIMAL APPROACH
+        //USED STACK WHICH WILL BE MIN STACK WHICH STORES THE MIN ELEMENT 
+        //INDEX ALWAYS ON THE TOP AND THEN TRAVERSING FROM THE BACK WE WILL 
+        //CHECK THE GIVEN CONDITION AND POP THE MIN INDEX
+
         stack<int>st;
         int res=0;
         for(int i=0;i<nums.size();i++){
+            //pushing all the indexs of the min ele
             if(st.empty() || nums[st.top()]>nums[i]){
                 st.push(i);
             }
