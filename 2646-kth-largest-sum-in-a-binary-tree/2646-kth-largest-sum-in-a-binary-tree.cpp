db46eaa0ee34dbf88c3226d051da0f0a2fad11ee
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2024-10-22 16:52:14 +0530

    addded comment

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
@@ -14,9 +14,10 @@ public:
     void levelorder(TreeNode* root,vector<long long>&v){
         queue<TreeNode*>q;
         q.push(root);
-        long long sum=0;
+        long long sum=0;    //consider the constraints [use long long]
         while(!q.empty()){
             int level=q.size();
+            //at each level
             while(level--){
                 TreeNode* cur = q.front();
                 q.pop();
@@ -25,15 +26,20 @@ public:
                 if(cur->right)q.push(cur->right);
             }
             v.push_back(sum);
-            sum=0;
+            sum=0;    //making it zero after pushing in vector as it will be adding 
         }
 
 
     }
     long long kthLargestLevelSum(TreeNode* root, int k) {
+        //Doing the level order traversal on the tree and 
+        //calculating the sum of the elements on the each level
         vector<long long>v;
         levelorder(root,v);
         sort(v.begin(),v.end());
+        
+        //finding kth largest sum
+
         for(int i=v.size()-1;i>=0;i--){
             k--;
             if(k==0){
