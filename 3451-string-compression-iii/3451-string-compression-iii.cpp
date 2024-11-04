695aa5f8d3edc232880b12d57f8327d7ec8ac770
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2024-11-05 00:13:41 +0530

    Update 3451-string-compression-iii.cpp

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string compressedString(string word) {
+        //Whenever a diff char occurrs add the count and the string into the comp but if last char occurs more than one time then we have to add it to the comp at last also and considering the 9 whenever the count goes above 9 add it to the comp and made the count to 1
         string comp ="";
         int count=1;
         char val=word[0];
