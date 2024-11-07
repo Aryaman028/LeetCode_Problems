0a4b2a0c9ba3d5b4203fab097c0e9bdad762645c
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2024-11-08 03:18:06 +0530

    Update 2356-largest-combination-with-bitwise-and-greater-than-zero.cpp

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -7,15 +7,15 @@ public:
             int d = num%2;
             if(d==1)res[c]++;
             c++;
-            // a += to_string(d);
             num/=2;
         }
-        // for(int i=0;i<a.length();i++){
-        //     if(a[i]=="1")res[i]++;
-        // }
     }
 
     int largestCombination(vector<int>& candidates) {
+        //Brute force - 
+        //taking the vector of 24 size to store all the indexes of the binary number and then we will increment the ele inside the index where the 1 is present in the 
+        // binary number like 100 1 is present at 2nd posi from left thus we will increment the 2nd index value hence we will 
+        // get at which position we are getting most number of 1s
         vector<int>res(24,0);
         for(int i=0;i<candidates.size();i++){
             toBinaryAndCount(candidates[i],res);
