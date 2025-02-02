6f90118a33ad6e120581f84d665b383dfff1c6b7
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-02-02 16:43:37 +0530

    Time: 0 ms (100%), Space: 11.8 MB (5.21%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        //its time complixety will be O(nlogn)
-
-        // vector<int>res;
-        // int k=0;
-        // bool flag =false;
-        // for(int i=1;i<nums.size();i++){
-        //     if(nums[i-1]>nums[i]){
-        //         k=i-1;              //position of the peak element
-        //         flag=true;         //it is getting a peak element
-        //     }
-        // }
-        // if(k==0 && !flag)k=nums.size()-1;       //that the array is already sorted
-
-        // //storing the elements in different array
-        // for(int i=k+1;i<nums.size();i++){
-        //     res.push_back(nums[i]);
-        // }
-        // for(int i=0;i<=k;i++){
-        //     res.push_back(nums[i]);
-        // }
-        // sort(nums.begin(),nums.end());
-        // if(k==nums.size())return true;
-        // if(res==nums)return true;
-        // return false;
-
-        //2nd APPROACH
-        int c=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]>nums[i+1]){
-                c++;
+        vector<int>v=nums;
+        sort(v.begin(),v.end());
+        int index =-1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]>nums[i]){
+                index=i;
+                break;
             }
         }
-        if(c==0)return true;
-        else if(c==1 && nums[0]>=nums[nums.size()-1])return true;
-        else return false;
-
-        return false;
-
-
+        if(index==-1)return true;
+        for(int i=0;i<nums.size();i++){
+            if(nums[(i+index) % nums.size()]!=v[i]){
+                return false;
+            }
+        }
+        return true;
 
     }
 };
