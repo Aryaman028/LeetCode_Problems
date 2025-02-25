a349bc1d09f68f0282688dab3eef764849a2b8a8
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-02-25 10:43:38 +0530

    Time: 0 ms (100%), Space: 17.8 MB (71.8%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    static bool comp(pair<int,int>a,pair<int,int>b){
-        return a.second > b.second;
-    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-
-        //1st approach USING PAIR TO STORE THE FREQUENCY OF THE ELEMENT AND THEN USING COMP SORTING TO SORT THE VECTOR BY THEIR FREQUENCY THEN TAKE THE FIRST K ELEMENTS
+        // 2ND APPROACH USING PRIORITY QUEUE WHICH WILL AUTOMATICALLY SET THE HIGHEST FREQUENCY AT THE TOP IF WE INSERT THE FREQUENCY IN THE FIRST POSITION OF PAIR
+      
         unordered_map<int,int>m;
         vector<int>res;
         for(auto ele:nums){
             m[ele]++;
         }
-        vector<pair<int,int>>v;
+        priority_queue<pair<int,int>>pq;
         for(auto ele:m){
-            v.push_back({ele.first,ele.second});
+            pq.push({ele.second,ele.first});
         }
-        sort(v.begin(),v.end(),comp);
 
         for(int i=0;i<k;i++){
-            res.push_back(v[i].first);
+            res.push_back(pq.top().second);
+            pq.pop();
         }
         return res;
 
