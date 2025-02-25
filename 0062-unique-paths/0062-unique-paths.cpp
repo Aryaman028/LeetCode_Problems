32d17a8247d4dad8cca8ae8ff6b8865e6d212e31
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-02-25 11:54:59 +0530

    Time: 0 ms (100%), Space: 9.5 MB (8.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,8 +9,25 @@ public:
         return dp[i][j];
     }
     int uniquePaths(int m, int n) {
+        // DP WITH MEMOIZATION
+        
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return ways(0,0,m,n,dp);
+
+        //DOING TABULATION
+        for(int row = 0; row < m; row++){
+            for(int col = 0 ; col < n ; col++){
+                //base case
+                if(row==0 || col==0){
+                    dp[row][col] = 1;
+                    continue;
+                }
+                dp[row][col]  = dp[row-1][col] + dp[row][col-1];
+                
+            }
+        }
+        return dp[m-1][n-1];
+
+        // return ways(0,0,m,n,dp);
         
     }
 };
