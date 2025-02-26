a1c57b56770c488a12b6b0ae60d7cce8fda1d84f
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-02-26 11:49:03 +0530

    Time: 1 ms (42.23%), Space: 16.4 MB (13.9%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    // int memo(int i,int j,int n,int m,vector<vector<int>>& grid){
-    //     if(i>m || j>n)return 0;
+    // int memo(int m,int n,vector<vector<int>>& grid,vector<vector<int>>&dp){
+    //     if(n<0 || m<0)return 1e9;
+    //     if(n==0 && m==0)return grid[0][0];
 
-    //     if(i==m-1 || j==n-1){
-    //         mini = min(mini,sum);
-    //         return
-    //     }
-    //     int sum =0;
+    //     if(dp[m][n]!=-1)return dp[m][n];
 
-    //     int x = memo(m-1,n,grid);
-    //     int y = memo(m,n-1,grid);
+    //     int x = grid[m][n] + memo(m-1,n,grid,dp);
+    //     int y = grid[m][n] + memo(m,n-1,grid,dp);
 
-    //     return sum + min(x,y);
+    //     return  dp[m][n] = min(x,y);
     // }
 
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        // dp[0][0] = grid[0][0];
+        // return memo(m-1,n-1,grid,dp);
 
+        // TABULATION 
         for(int row =0; row<m; row++){
             for(int col = 0; col < n; col++){
 
                 if(row==0 && col==0)dp[row][col] =grid[row][col];
-                else if(row==0){
-                    dp[row][col] = grid[row][col] + dp[row][col-1];
-                }else if(col==0){
-                    dp[row][col] = grid[row][col] + dp[row-1][col];
-                }else{
-                    dp[row][col] = grid[row][col] + min(dp[row-1][col],dp[row][col-1]);
+                else{
+                    int up =1e7,left=1e7;
+                    if(row>0){
+                        up = dp[row-1][col];
+                    }
+                    if(col>0){
+                        left = dp[row][col-1];
+                    }
+                    dp[row][col] = grid[row][col] + min(up,left);
                 }
             }
         }
