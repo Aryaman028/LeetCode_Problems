500ffc4a4de63cb241e01ae5585dc511d5ae178d
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-01 19:31:57 +0530

    Time: 0 ms (100%), Space: 12.9 MB (99.53%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
-        vector<int>res(n,0);
-        int countZero = 0;
 
         for(int i = 0; i < n - 1; i++){
             if(nums[i] == nums[i + 1]){
@@ -11,12 +9,25 @@ public:
                 nums[i+1] = 0;
             }
         }
-        int j=0;
-        for(int i = 0;i < n; i++){
-            if(nums[i] != 0)res[j++] = nums[i] ;
-
+        int zeroPosi=-1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                if(zeroPosi == -1) zeroPosi = i;
+            } 
+            else{
+                if(zeroPosi != -1){
+                    swap(nums[zeroPosi],nums[i]);
+                    zeroPosi++;
+                }
+            }
         }
-        return res;
+        // int j=0;
+
+        // for(int i = 0;i < n; i++){
+        //     if(nums[i] != 0)res[j++] = nums[i] ;
+
+        // }
+        return nums;
         
     }
 };
