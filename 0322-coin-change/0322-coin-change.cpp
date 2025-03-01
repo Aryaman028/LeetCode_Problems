5057a505d60dac79d5eeccdc447cb25f8f35dac3
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-02 00:22:45 +0530

    Time: 54 ms (20.35%), Space: 48.7 MB (39.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,56 @@
 class Solution {
 public:
     int memo(int n , int amount , vector<int>&coins,vector<vector<int>>&dp){
-        //important base case
-        if( n == 0 ){
+        if(n==0){
             if(amount % coins[n] == 0) return amount / coins[n];
             return 1e9;
-        } 
-
+        }
+        
         if(dp[n][amount] != -1) return dp[n][amount];
-
-        //pick
         int pick = INT_MAX;
-        if(amount >= coins[n]) pick = 1 + memo(n,amount - coins[n], coins , dp);
 
-        int notPick = memo(n-1 , amount, coins, dp);
+        if(amount >= coins[n]){
+            pick = 1 + memo(n, amount - coins[n], coins, dp);
+        }
+        int notPick = memo(n-1, amount , coins, dp);
 
-        return dp[n][amount] =  min(pick,notPick);
-
+        return dp[n][amount] = min(pick,notPick);
         
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         sort(coins.begin(),coins.end());
 
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        // vector<vector<int>>dp(n,vector<int>(amount+1,-1));
 
-        int result = memo(n-1,amount,coins,dp);
+        // int result = memo(n-1,amount,coins,dp);
+        // if(result >= 1e9 ) return -1;
+        // if(amount == 0) return 0;
+
+
+        //TABULATION
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        //BASE CASE
+        for(int i = 0;i <= amount ;i++){
+            
+            if(i % coins[0] == 0){
+                dp[0][i] = i / coins[0];
+            }else dp[0][i] = 1e9;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= amount ;j++){
+
+                int pick = (j >= coins[i]) ? 1 + dp[i][j - coins[i]] : INT_MAX ;
+            
+                int notPick = dp[i-1][j];
+
+                dp[i][j] = min(pick,notPick);
+            }
+        }
+        int result = dp[n-1][amount];
+
         if(result >= 1e9 ) return -1;
-        if(amount == 0) return 0;
-
         return result;
         
     }
