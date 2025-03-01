1dc5e63f90ee69c37ab99c7727048b7242f299c2
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-02 02:13:05 +0530

    Time: 29 ms (33.68%), Space: 46.1 MB (13.73%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
+
+    // MEMOIZATION 
     int memo(int ind , int amount, vector<int>&coins ,vector<vector<int>>&dp){
-        if(amount == 0) return 1;
+        //most important base case
         if(ind == 0){
-                if(amount >= coins[0]){
-                    return memo(ind,amount - coins[ind], coins,dp);
-                }
-                return 0;
+                return (amount % coins[0] == 0);
         }
         if(dp[ind][amount] != -1) return dp[ind][amount];
 
@@ -19,8 +18,28 @@ public:
 
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return memo(n-1,amount,coins,dp);
-        
+        // vector<vector<long long>>dp(n,vector<long long>(amount+1,0));
+        // return memo(n-1,amount,coins,dp);
+
+        //TABULATION
+
+        // BASE CASE
+        // on ind == 0 amount can be anything, thus putting the value which is returning in the base case as value
+   
+        vector<vector<unsigned long long>> dp(n, vector<unsigned long long>(amount + 1, 0));
+
+        for (int i = 0; i <= amount; i++) {
+            dp[0][i] = (i % coins[0] == 0);
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int t = 0; t <= amount; t++) {
+                unsigned long long notPick = dp[ind - 1][t];  // Not using current coin
+                unsigned long long pick = (t >= coins[ind]) ? dp[ind][t - coins[ind]] : 0;  // Using current coin
+
+                dp[ind][t] = pick + notPick;
+            }
+        }
+        return dp[n - 1][amount];
     }
 };
