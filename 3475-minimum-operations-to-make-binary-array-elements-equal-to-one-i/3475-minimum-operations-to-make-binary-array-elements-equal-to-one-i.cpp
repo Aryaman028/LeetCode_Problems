510035403bfb98504e9eed19fb71bb953af19b9f
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-19 20:36:42 +0530

    Time: 3 ms (89.05%), Space: 140 MB (74.5%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -21,17 +21,18 @@ public:
 
         int n = nums.size();
         int c = 0;
-        for(int i = 0; i < n - 2; i++){
+        for(int i = 0; i < n; i++){
             if(nums[i] == 0){
                 nums[i] ^= 1;
+                if((i+1) >= n || (i+2) >= n){
+                    return -1;
+                }
                 nums[i + 1] ^= 1;
                 nums[i + 2] ^= 1;
                 c++;
             }
         }
-        for(auto ele: nums){
-            if(ele == 0)return -1;
-        }
+       
         return c;
     }
 };
