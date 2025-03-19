9571c39a399a85516ec018ed0cd34f9fce05baa6
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-19 20:34:36 +0530

    Time: 16 ms (12.97%), Space: 140 MB (47.55%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
+        //BRUTE FORCE COSTING TC -> O(N * 3);
+        //WHENEVER WE WILL BE HAVING ZERO THEN FLIP 3 ELEMENTS
+
+        // int n = nums.size();
+        // int c = 0;
+        // for(int i = 0; i < n - 2; i++){
+        //     if(nums[i] == 0){
+        //         for(int j = i; j < i + 3; j++){
+        //             nums[j] ^= 1;
+        //         }
+        //         c++;
+        //     }
+        // }
+        // for(auto ele: nums){
+        //     if(ele == 0)return -1;
+        // }
+        // return c;
+
         int n = nums.size();
         int c = 0;
         for(int i = 0; i < n - 2; i++){
             if(nums[i] == 0){
-                for(int j = i; j < i + 3; j++){
-                    nums[j] ^= 1;
-                }
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
                 c++;
             }
         }
