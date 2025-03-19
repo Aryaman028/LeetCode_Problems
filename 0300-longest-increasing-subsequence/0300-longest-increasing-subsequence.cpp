604bf1f607ccabb5f621c6ca3543f63bf60761a1
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-20 00:09:49 +0530

    Time: 65 ms (55.64%), Space: 14.4 MB (45.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -22,24 +22,41 @@ public:
         // return solve(0,-1,nums,dp);
         
         //using memoization now i am doing tabulation
+        // int n = nums.size();
+        // vector<vector<int>>dp(n + 1,vector<int>(n+1,0));
+
+        // //conside the shift we have done for prev
+        // for(int i = n - 1; i >= 0 ; i--){
+        //     for(int prev = i - 1; prev >= -1; prev--){
+        //         int y=0;
+        //         if(prev==-1 || nums[i]>nums[prev]){
+        //             //shifting the coordinates thus we are adding 1 to i in the column
+        //             y = 1 + dp[i + 1][i + 1];
+        //         }
+        //         int x = dp[i+1][prev + 1];
+        //         dp[i][prev+1] = max(x,y);
+
+        //     }
+        // }
+        // return dp[0][0];
+
+        //TOTALLY DIFFERENCT TABULATION APPROACH CANT BE BUILD BY OU OWN INTUITION
+
+        // for each index count the longest increasing subsequence till that index
+
         int n = nums.size();
-        vector<vector<int>>dp(n + 1,vector<int>(n+1,0));
+        vector<int>dp(n,1);
 
-        //conside the shift we have done for prev
-        for(int i = n - 1; i >= 0 ; i--){
-            for(int prev = i - 1; prev >= -1; prev--){
-                int y=0;
-                if(prev==-1 || nums[i]>nums[prev]){
-                    //shifting the coordinates thus we are adding 1 to i in the column
-                    y = 1 + dp[i + 1][i + 1];
+        int maxi = INT_MIN;
+        for(int i = 0; i < n; i++){
+            for(int prev = 0; prev < i; prev++){
+                if(nums[prev] < nums[i]){
+                    dp[i] = max(1 + dp[prev], dp[i]);
                 }
-                int x = dp[i+1][prev + 1];
-                dp[i][prev+1] = max(x,y);
-
             }
+            maxi = max(dp[i],maxi);
         }
-        return dp[0][0];
-
-
+        
+        return maxi;
     }
 };
