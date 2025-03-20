b853675e7d53089473aae55f6911b9128c648171
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-21 01:24:01 +0530

    Time: 0 ms (100%), Space: 16.6 MB (93.91%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -19,6 +19,9 @@ public:
             }
         }
         cout<<freshOranges<<endl;
+
+        vector<pair<int,int>>dir = {{1,0},{0,1},{-1,0},{0,-1}};
+
         int time = 0;
         while(!q.empty()){
             int l = q.size();
@@ -29,35 +32,50 @@ public:
                 int row = p.first;
                 int col = p.second;
                 q.pop();
+                
+                // use this method if you want a question for directions
+                for(auto [dr,dc] : dir){
+                    int newCol = col + dc;
+                    int newRow = row + dr;
 
-                //down
-                if(row + 1 < m && grid[row + 1][col] == 1){
-                    grid[row + 1][col] = 2;
-                    change = true;
-                    q.push({row + 1,col});
-                    freshOranges--;
+                    if(newCol >= 0 && newRow >= 0 && newCol < n && newRow < m && grid[newRow][newCol] == 1){
+                        grid[newRow][newCol] = 2;
+                        change = true;
+                        q.push({newRow,newCol});
+                        freshOranges--;
+                    }
                 }
-                //up
-                if(row - 1 >= 0 && grid[row - 1][col] == 1){
-                    grid[row - 1][col] = 2;
-                    change = true;
-                    q.push({row - 1,col});
-                    freshOranges--;
-                }
-                //right
-                if(col + 1 < n && grid[row][col + 1] == 1){
-                    grid[row][col + 1] = 2;
-                    change = true;
-                    q.push({row,col + 1});
-                    freshOranges--;
-                }
-                //left
-                if(col - 1 >= 0 && grid[row][col - 1] == 1){
-                    grid[row][col - 1] = 2;
-                    change = true;
-                    q.push({row,col - 1});
-                    freshOranges--;
-                }
+
+                //basic way to write all the 4 directions
+
+            //     //down
+            //     if(row + 1 < m && grid[row + 1][col] == 1){
+            //         grid[row + 1][col] = 2;
+            //         change = true;
+            //         q.push({row + 1,col});
+            //         freshOranges--;
+            //     }
+            //     //up
+            //     if(row - 1 >= 0 && grid[row - 1][col] == 1){
+            //         grid[row - 1][col] = 2;
+            //         change = true;
+            //         q.push({row - 1,col});
+            //         freshOranges--;
+            //     }
+            //     //right
+            //     if(col + 1 < n && grid[row][col + 1] == 1){
+            //         grid[row][col + 1] = 2;
+            //         change = true;
+            //         q.push({row,col + 1});
+            //         freshOranges--;
+            //     }
+            //     //left
+            //     if(col - 1 >= 0 && grid[row][col - 1] == 1){
+            //         grid[row][col - 1] = 2;
+            //         change = true;
+            //         q.push({row,col - 1});
+            //         freshOranges--;
+            //     }
             }
             if(change == true)time++;
 
