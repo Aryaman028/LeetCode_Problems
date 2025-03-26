2b16d5189873bf9971dbd6d10a6c7d611b2d1871
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-03-26 05:32:56 +0530

    Time: 63 ms (42.92%), Space: 131 MB (56.16%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,45 +1,58 @@
 class Solution {
 public:
-    void bfs(int node,vector<vector<int>> &adj,vector<int> &comp,vector<bool> &vis){
-        queue<int> q;
-        q.push(node);
-        vis[node]=true;
+    void dfs(int node,vector<vector<int>>&adj, vector<bool>&visited, vector<int>&v){
+        visited[node] = true;
 
-        while(!q.empty()){
-            int n=q.front();
-            q.pop();
-            comp.push_back(n);
-            for(auto it:adj[n]){
-                if(!vis[it]){
-                    q.push(it);
-                    vis[it]=true;
-                }
+        v.push_back(node);
+
+        for(auto ele : adj[node]){
+            if(!visited[ele]){
+                dfs(ele, adj, visited, v);
             }
         }
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for(auto it:edges){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
-        int ans=0;
-        vector<bool> vis(n,false);
-        for(int i=0;i<n;i++){
 
-            if(!vis[i]){
-                vector<int> comp;
-                bfs(i,adj,comp,vis);
-                bool iscomp=true;
-                for(auto it: comp){
-                    if(adj[it].size()!=comp.size()-1){
-                        iscomp=false;
+        int l = edges.size();
+        vector<int>inDegree(n,0);
+        vector<vector<int>>adj(n);
+
+        for(int i = 0; i < l; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            inDegree[u]++;
+            inDegree[v]++;
+        }
+
+        vector<bool>visited(n,false);
+        int count = 0;
+
+        //performing DFS on every node and counting the nodes in the component and then using inDegree we know which node have how much indegree thus compare the nodes in a component there indegree should be [n - 1] where n is the number of nodes in that component
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                vector<int>eleInComponent;
+                dfs(i,adj,visited,eleInComponent);
+
+                // got the elements into the vector
+                int totalNodes = eleInComponent.size();
+                bool complete = true;
+
+                //check the indegree of each node present in the current component and mark it complete or not
+                for(auto ele : eleInComponent){
+                    if(inDegree[ele] != totalNodes - 1){
+                        complete = false;
                         break;
                     }
                 }
-                if(iscomp)ans++;
+                if(complete){
+                    count++;
+                }
             }
+
         }
-        return ans;
+        return count;
+        
     }
 };
