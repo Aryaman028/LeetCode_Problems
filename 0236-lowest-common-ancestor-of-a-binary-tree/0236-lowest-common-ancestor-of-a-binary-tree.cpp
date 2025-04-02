abae073e9974045ec621834e9f2864e92a254bed
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-04-02 11:48:43 +0530

    Time: 17 ms (21.44%), Space: 18.5 MB (11.23%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,24 +9,20 @@
  */
 class Solution {
 public:
-    bool path(TreeNode* root, TreeNode* p,vector<TreeNode*>&v){
-            if(root==NULL)return false;
-            if(root->val==p->val){
-                v.push_back(root);
-                return true;
-            }
-
-            bool res = path(root->left,p,v) || path(root->right,p,v);
-            if(res){
-                v.push_back(root);
-                return true;
-            }
-            
-            return false;
+    bool path(TreeNode* root, TreeNode* p, vector<TreeNode*>&v){
+        if(root == NULL)return false;
+        if(root == p){
+            v.push_back(root);
+            return true;
         }
-
+        bool res = path(root->left, p, v) || path(root->right, p, v);
+        if(res){
+            v.push_back(root);
+            return true;
+        }
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        //in this we have to find the path of the each ele and store it in an array and then compare both the arrays.
         vector<TreeNode*>v1;
         vector<TreeNode*>v2;
         path(root,p,v1);
@@ -35,18 +31,13 @@ public:
         reverse(v1.begin(),v1.end());
         reverse(v2.begin(),v2.end());
 
-        int i=0,j=0;
-        //using pointers check where you found the common ancestor
-        TreeNode* ans = new TreeNode();
-        while(i<v1.size() && j<v2.size()){
-            if(v1[i]==v2[j]){
-                ans = v1[i];
-                i++;
-                j++;
+        int i = 0;
+        while(i < v1.size() && i < v2.size()){
+            if(v1[i]!=v2[i]){
+                break;
             }
-            else break;
+            i++;
         }
-        return ans;
-
+        return v1[i - 1];
     }
 };
