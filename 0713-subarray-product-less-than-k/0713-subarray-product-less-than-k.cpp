8e4ff1f11778ea350fa8f1b5a0cb598cd4c13c1f
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2025-04-05 02:04:30 +0530

    Time: 0 ms (100%), Space: 65.2 MB (66.12%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -4,20 +4,22 @@ public:
         //I WILL BE USING SLIDING WINDOW AND GO TO THAT INDEX TILL WHERE THE SUBARRAY IS VALID THEN I WILL COUNT THE LENGTH OF THE SUBARRAY THEN COUNT THE SUBARRAYS IT CAN FORM ADD IT TO THE RES;
 
         int i = 0, j = 0;
+        //edge case
+        
         if(k <= 1)return 0;
         int n = nums.size();
         long long pro = 1;
         int count = 0;
+
         while(j < n){
             pro *= nums[j];
+
             while(i < n && pro >= k){
                 pro = pro / nums[i];
                 i++;
             }
-            if(i <= j){
-                int len = j - i + 1;
-                count += len;
-            }
+            int len = j - i + 1;
+            count += len;
             j++;
         }
         return count;
